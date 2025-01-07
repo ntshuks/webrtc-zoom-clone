8cf18e15b3b36859d4c9d31e9c466973e4662141
ntshuks <ntshuks@gmail.com>
2025-01-07 17:19:37 +0200

    initial commit

---
+++ public/script.js
@@ -0,0 +1,208 @@
+const socket = io.connect('https://192.168.1.129:5001');
+
+const join = document.getElementById('username-form');
+const usernameInput = document.getElementById('username');
+const alluserlist = document.getElementById("alluserlist");
+const callform = document.getElementById("call-form");
+const answerform = document.getElementById("answer-form");
+const hangupform = document.getElementById("hangup-form");
+const hangupbutton = document.getElementById("hangup-button");
+const answerbutton = document.getElementById("answer-button");
+const callbutton = document.getElementById("call-button");
+const calleeInput = document.getElementById("callee");
+const localvideoEl = document.querySelector("#localvideo");
+const remotevideoEl = document.querySelector("#remotevideo"); 
+let users;
+
+const constraints = {
+   video: true,
+   audio:true
+};
+let caller_id; // id of person initialing call
+let callee_id; // id of person receiving call
+let callerice_count = 0;
+let localstream;
+let remotestream;
+let peerconnection;
+let callericecandidates=[];
+let calleeicecandidates=[];
+
+const configuration = {'iceServers': [
+   {'urls': [
+      'stun:stun.l.google.com:19302',
+     /* 'stun:stun1.l.google.com:19302', */
+   ]}]};
+
+join.addEventListener('submit', (e) =>{
+   e.preventDefault();
+   username = usernameInput.value;
+   socket.emit('join', (username));
+});
+
+socket.on('welcome', ({userlist,id}) =>{
+   console.log('Welcome to the app');
+});
+
+callform.addEventListener('submit', (e) =>{
+   e.preventDefault();
+  const result = finduser2(calleeInput.value);
+  if (!result) {
+   alert('No such user'); 
+  } else {
+   callee_id = result.id;
+   const result2 = finduser2(usernameInput.value);
+   caller_id = result2.id;
+   if (callee_id == caller_id) {
+      alert('You cannot call yourself');
+   } else {
+   makeCall(caller_id,callee_id);
+   callbutton.disabled = true;
+   }
+  }
+});
+
+async function makeCall(caller_id,callee_id) {   
+   await getlocalmedia(); 
+   peerconnection = new RTCPeerConnection(configuration);
+   remotestream = new MediaStream();
+   remotevideoEl.srcObject = remotestream;
+   // Add local tracks to lcaostream
+   localstream.getTracks().forEach(track =>{
+     peerconnection.addTrack(track,localstream);
+   });
+   peerconnection.addEventListener('icecandidate', (event) => {
+      if (event.candidate) {
+        // const iceCandidateObj = {iceCandidate:event.candidate}
+         const iceCandidateObj = (event.candidate);
+         console.log('*** iceCandidateObj ***');
+         console.log(iceCandidateObj);
+         socket.emit('caller-ice-candidate', (iceCandidateObj));
+         }
+      });
+   peerconnection.addEventListener('track',e=>{
+   console.log('Got a track from callee');
+   console.log(e);
+   e.streams[0].getTracks().forEach(track=>{
+       remotestream.addTrack(track,remotestream);
+   });
+ });
+   const offer = await peerconnection.createOffer();
+   await peerconnection.setLocalDescription(offer);
+   socket.emit('newoffer',({offer, callee_id, caller_id}));
+}
+
+socket.on('getoffer', ({offer, callee_id, caller_id}) =>{
+   // We have the offer but nothing should happen until callee answers
+   answerbutton.disabled = false;
+   hangupbutton.disabled = false;
+   answerbutton.style.backgroundColor = "#90ee90";
+   answerform.addEventListener('submit', async (e) => {
+      e.preventDefault();
+     answercall(offer,caller_id,callee_id);
+     answerbutton.disabled = true;
+     callbutton.disabled = true;
+   });
+});
+
+async function answercall(offer, caller_id,callee_id) {
+   await getlocalmedia();
+   //await callee_createpeerconnection(offer);
+   peerconnection = new RTCPeerConnection(configuration);
+   remotestream = new MediaStream();
+   remotevideoEl.srcObject = remotestream;
+   // Get local tracks
+   localstream.getTracks().forEach(track =>{
+   peerconnection.addTrack(track,localstream);
+   });
+   //Listen out for tracks from caller
+   peerconnection.addEventListener('track',e=>{
+      console.log('Got a track from callee');
+      console.log(e);
+      e.streams[0].getTracks().forEach(track=>{
+          remotestream.addTrack(track,remotestream);
+      });
+    });
+  await peerconnection.setRemoteDescription(offer);
+ 
+   peerconnection.addEventListener('icecandidate', (event) => {
+         const iceCandidateObj = (event.candidate);
+         console.log('*** iceCandidateObj ***');
+         console.log(iceCandidateObj);
+         socket.emit('callee-ice-candidate', (iceCandidateObj));
+   });
+   const answer = await peerconnection.createAnswer();
+    await peerconnection.setLocalDescription(answer);
+    console.log('Answer below');
+    console.log(answer);
+
+   // Get caller icecandidates DO this with Rob's crafty emitWithAck idea
+   callericecandidates = await socket.emitWithAck('newanswer', answer);
+   callericecandidates.forEach((element) =>{
+   peerconnection.addIceCandidate(element)
+     .then (() => console.log('>>> added caller ice candidates to callee <<<'))
+     .then (() => console.log(element))
+     .catch((error) => console.log(error))
+    }); 
+   }
+
+socket.on('getanswer', (answer) =>{
+addanswer(answer);
+});
+
+async function addanswer(answer) {
+   await peerconnection.setRemoteDescription(answer);
+   hangupbutton.disabled = false;
+}
+
+socket.on('received-callee-IceCandidateFromServer',(iceCandidateObj) =>{
+   console.log('++++ added callee ice candidate to caller socket ++++');
+   peerconnection.addIceCandidate(iceCandidateObj)
+   .then (() => console.log('Callee Ice candidate added - below'))
+   .catch ((error) => {
+      console.log(`Error adding ice candidte: ${error.name}`);
+   })
+});
+
+async function getlocalmedia(e) {
+   try {
+   const stream = await navigator.mediaDevices.getUserMedia(constraints);
+   localvideoEl.srcObject = stream;
+   localstream = stream;
+} catch (e) {
+  console.log(e); }
+}
+
+hangupform.addEventListener('submit', (e) => {
+   // Doesn't matter who triggered event, send message to server to io to call both clients
+   e.preventDefault();
+   socket.emit('hangup')
+});
+
+socket.on('closeconnection', () =>{
+   peerconnection.close();
+   location.reload();
+   alert("Bye...");
+});
+
+socket.on("joinerror", (joined) => {
+   alert('You have already joined - refresh page to start again');
+});
+
+socket.on('updateuserlist', (userlist) => {
+   users = userlist;
+   alluserlist.innerText='';
+   let item = document.createElement('li');
+   item.textContent = 'Users: ';
+   alluserlist.appendChild(item);
+   for (i=0 ; i < users.length ; i++) {
+       item = document.createElement('li');
+        item.textContent = users[i].username;
+        alluserlist.appendChild(item);
+   }
+});
+
+function finduser2(username) {
+   // search by username
+   const searchObj = users.find((user) => user.username === username);
+   return (searchObj);
+}
---
+++ server.js
@@ -0,0 +1,132 @@
+/*
+Create video chat app usng socket.io and webrtc - no front end framework
+Heavily influenced by Robert Bunch's Webrtc-starter video & app
+*/
+const fs = require('fs');
+const key = fs.readFileSync('./certs/server-cert-key.pem');
+const cert = fs.readFileSync('./certs/server-cert.pem')
+const express = require('express');
+const app = express();
+const server = require('node:https').createServer({key,cert},app);
+require ('dotenv').config();
+const io = require('socket.io')(server, {
+    cors: {
+        origin: [
+            'https://192.168.1.129'
+        ],
+        methods: ["Get","POST"]
+    }
+});
+const PORT = process.env.port || 5001;
+
+// count to see how many candidates
+let c_count = 0;
+let server_caller_ice = [];
+let server_caller_id;
+let server_callee_id;
+
+//Declare UserList - array of user objects
+let userlist = [];
+
+// Set Static Folder - public
+app.use(express.static('public'));
+
+//Routes - only one
+app.get('/', (req,res) => {
+    res.render('index.html');
+});
+
+app.post('/', (req,res) => {
+    res.render('index.html');
+});
+
+// Socket io logic
+io.on('connection', (socket) => {
+    console.log(`Socket id: ${socket.id}`);
+     const id = socket.id;
+     // When user has submitted username
+     socket.on('join', (username) => {
+
+     // Check to see if user already joined - reject if so
+     const result = findUser(id);
+     if(result) {
+    const joined = result.id;
+    socket.emit("joinerror", joined);
+    } else {
+    const newuser = {username, id}
+    userlist.push(newuser);
+    console.log(userlist);
+    // send back userlist to front end
+    socket.emit('welcome', ({userlist,id}));
+    // send room user list to whole room
+    io.emit("updateuserlist", userlist); }
+     });
+
+    socket.on('newoffer', ({offer, caller_id, callee_id}) => {
+        server_caller_id = caller_id; // to save passing these back and forth
+        server_callee_id = callee_id;
+        console.log(`caller ${server_caller_id}  callee ${server_callee_id}`);
+        console.log(offer);
+        socket.to(server_callee_id).emit('getoffer', ({offer,server_caller_id,server_callee_id}));
+     });
+
+     socket.on('caller-ice-candidate', (iceCandidateObj) => {
+        server_caller_ice[c_count] = iceCandidateObj;
+        c_count++;
+        console.log('caller ice candidates: count ' + c_count);
+        console.log(server_caller_ice);
+      });
+
+     socket.on('callee-ice-candidate', (iceCandidateObj) => {
+       console.log('Sending callee ice candidates to caller: ' + server_caller_id);
+       console.log(iceCandidateObj);
+       socket.to(server_caller_id).emit('received-callee-IceCandidateFromServer',(iceCandidateObj));
+     });
+
+    socket.on('newanswer', (answer, Ackcb) => {
+        Ackcb(server_caller_ice); //this is sent back to callee
+    // Now send answer to caller
+    socket.to(server_caller_id).emit('getanswer', (answer));
+    answerreceived = true;
+    console.log(`inside new answer. answerrecieved: ${answerreceived}`);
+    });
+
+    socket.on('hangup', () => {
+       io.emit('closeconnection');
+    });
+
+     socket.on('disconnect', () =>{
+     // find user an dremove from userlist
+     console.log(`socket that disconnected: ${socket.id}`);
+     const index = findUserIndex(socket.id);
+     const user = findUser(socket.id);
+     console.log("print out index of user to be removed");
+     console.log(index);
+     // remove user
+     if (index !== -1) {
+        console.log('INDEX to be spliced');
+        console.log(index);
+        userlist.splice(index,1);
+        console.log("User List after splice");
+        console.log(userlist);
+        io.emit("updateuserlist", userlist);
+    }
+    console.log(`User disconnected, socket.id: ${socket.id}`);
+
+     });
+});
+ 
+function findUserIndex(id) {
+    // search to find index of element in array
+    const index = userlist.findIndex((user) => user.id === id);
+    return(index);
+}
+
+function findUser(id) {
+    // search by id
+    const searchObj = userlist.find((user) => user.id === id);
+    return (searchObj);
+}
+
+//Listen on selected port
+server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
